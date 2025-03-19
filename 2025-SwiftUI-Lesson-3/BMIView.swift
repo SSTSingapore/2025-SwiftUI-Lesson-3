5a98a50d742519022f3c469532e957881a81d685
Stanley Sim <simyseng@gmail.com>
2025-03-19 15:14:59 +0800

    NavigationStack

---
+++ 2025-SwiftUI-Lesson-3/BMIView.swift
@@ -0,0 +1,18 @@
+//
+//  BMIView.swift
+//  2025-SwiftUI-Lesson-3
+//
+//  Created by Stanley Sim on 19/3/25.
+//
+
+import SwiftUI
+
+struct BMIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BMIView()
+}
--- 2025-SwiftUI-Lesson-3/ContentView.swift
+++ 2025-SwiftUI-Lesson-3/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Image("L3.001")
+                    .resizable()
+                NavigationLink("BMI Calculator") {
+                    BMIView().navigationTitle("BMI Calculator")
+                }
+                .padding()
+                .background(.white)
+                .cornerRadius(5)
+            }
+            .ignoresSafeArea()
         }
-        .padding()
     }
 }
 
