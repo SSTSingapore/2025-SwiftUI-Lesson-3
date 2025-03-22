0609c97410134850526c003be5b309cce3050f96
Stanley Sim <sim_yong_seng_stanley@sst.edu.sg>
2025-03-22 13:18:50 +0800

    Part-2

--- 2025-SwiftUI-Lesson-3/BMIView.swift
+++ 2025-SwiftUI-Lesson-3/BMIView.swift
@@ -8,8 +8,56 @@
 import SwiftUI
 
 struct BMIView: View {
+    @State private var height: Double = 1.7
+    @State private var weight: Double = 59
+    @State private var bmi: Double = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("Height")
+                .padding()
+            TextField("Height", value: $height, format: .number)
+                .padding()
+                .multilineTextAlignment(.center)
+                .disableAutocorrection(true)
+                .keyboardType(.decimalPad)
+                .textFieldStyle(.roundedBorder)
+        }
+        HStack {
+            Text("Weight")
+                .padding()
+            TextField("Weight", value: $weight, format: .number)
+                .padding()
+                .multilineTextAlignment(.center)
+                .disableAutocorrection(true)
+                .keyboardType(.decimalPad)
+                .textFieldStyle(.roundedBorder)
+            
+        }
+        Button("Calculate"){
+            bmi = (weight / pow(height,2))
+        }
+        .padding()
+        
+        Text("\(bmi, specifier: "%.2f")")
+            .padding()
+        Text(getHealthRisk(bmi))
+            .padding()
+    }
+}
+
+func getHealthRisk(_ bmi: Double) -> String {
+    switch bmi {
+    case 0 ..< 18.5:
+        return "Possible nutritional deficiency and osteoporosis."
+    case 18.5 ..< 22.9:
+        return "Low risk (healthy range)."
+    case 22.9 ..< 27.4:
+        return "Moderate risk of developing heart disease, high blood pressure, stroke, diabetes mellitus"
+    case 27.4 ..< 40:
+        return "High risk of developing heart disease, high blood pressure, stroke, diabetes mellitus. Metabolic Syndrome."
+    default:
+        return ""
     }
 }
 
